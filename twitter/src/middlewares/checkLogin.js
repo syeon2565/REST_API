33e06ea680c3f0b094092b18324d2bcc9efa51d3
seongyeon <ysy2565@naver.com>
2022-02-12 16:11:29 +0900

    feat: jwt 추가

--- twitter/src/middlewares/checkLogin.js
+++ twitter/src/middlewares/checkLogin.js
@@ -1,7 +1,18 @@
 const checkLogin = (req, res, next) => {
-  if (!req.header("x-user-id")) {
+  try {
+    req.decode = verify(req.headers.authorization, process.env.JWT_SECRET);
+    return next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      // 유효기간 초과
+      return res.status(419).json({
+        code: 419,
+        message: "토큰이 만료되었습니다",
+      });
+    }
     return res.status(401).json({
-      error: "Please Login",
+      code: 401,
+      message: "유효하지 않은 토큰입니다",
     });
   }
 };
